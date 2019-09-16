51885254fba8e599c3a37f57a528bfa1e0864c20
rossanthony <ross@velo33.com>
2019-09-15 20:03:57 -0400

    Add new methods to redis service

--- src/RedisService.ts
+++ src/RedisService.ts
@@ -22,6 +22,34 @@ export class RedisService {
         });
     }
 
+    /**
+     * Check if value is member of set.
+     * @return integer
+     * 1 if value exists in the set
+     * 0 if not present
+     */
+    public async sismember(key: string, value: any): Promise<number> {
+        return new Promise((resolve) => {
+            this.redisClient.sismember(key, value, (error: RedisError | null, reply: number) => {
+                if (error) {
+                    console.log('Redis error thrown for sismember:', key);
+                }
+                resolve(reply || 0);
+            });
+        });
+    }
+
+    public async smembers(key: string): Promise<string[]> {
+        return new Promise((resolve) => {
+            this.redisClient.smembers(key, (error: RedisError | null, reply: string[]) => {
+                if (error) {
+                    console.log('Redis error thrown for smembers:', key);
+                }
+                resolve(reply || []);
+            });
+        });
+    }
+
     public async scard(key: string): Promise<number> {
         return new Promise((resolve) => {
             this.redisClient.scard(key, (error: RedisError | null, reply: number) => {
